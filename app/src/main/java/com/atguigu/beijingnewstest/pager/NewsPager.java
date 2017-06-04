0f82ae3087b6497167dbdbd70210500316bd4f2d
lianshuideyu <834893479@qq.com>
2017-06-04 15:35:40 +0800

    新闻中心数据请求&json数据解析

--- app/src/main/java/com/atguigu/beijingnewstest/pager/NewsPager.java
+++ app/src/main/java/com/atguigu/beijingnewstest/pager/NewsPager.java
@@ -7,6 +7,13 @@ import android.view.Gravity;
 import android.widget.TextView;
 
 import com.atguigu.beijingnewstest.base.BasePager;
+import com.atguigu.beijingnewstest.domain.NewsCenterBean;
+import com.atguigu.beijingnewstest.utils.ConstantUtils;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
+
+import okhttp3.Call;
 
 /**
  * Created by Administrator on 2017/6/2.
@@ -34,5 +41,38 @@ public class NewsPager extends BasePager {
 
         //添加到Fragment布局上
         flContent.addView(textView);
+
+        //联网获取数据
+        getDataFromNet();
+    }
+
+    private void getDataFromNet() {
+        String url = ConstantUtils.NEWSCENTER_PAGER_URL;
+        OkHttpUtils
+                .get()
+                .url(url)
+//                .addParams("username", "hyman")
+//                .addParams("password", "123")
+                .build()
+                .execute(new StringCallback() {
+
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG","NewsPager联网失败" + e.getMessage());
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+                        Log.e("TAG","NewsPager联网成功" + response);
+                        //解析数据
+                        processData(response);
+                    }
+                });
+    }
+
+    private void processData(String json) {
+        NewsCenterBean newsCenterBean = new Gson().fromJson(json, NewsCenterBean.class);
+        Log.e("TAG","数据解析==" + newsCenterBean.getData().get(0).getTitle());
+
     }
 }
