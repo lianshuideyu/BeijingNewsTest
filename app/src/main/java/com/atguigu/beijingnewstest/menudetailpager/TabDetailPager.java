6dfb6baa90e086e2bd4ed212b5836b49f909d4f0
lianshuideyu <834893479@qq.com>
2017-06-05 14:54:16 +0800

    页签详情数据访问和解析&顶部新闻数据填充

--- app/src/main/java/com/atguigu/beijingnewstest/menudetailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/beijingnewstest/menudetailpager/TabDetailPager.java
@@ -1,9 +1,12 @@
 package com.atguigu.beijingnewstest.menudetailpager;
 
 import android.content.Context;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -13,9 +16,14 @@ import com.atguigu.beijingnewstest.base.MenuDetailBasePager;
 import com.atguigu.beijingnewstest.domain.NewsCenterBean;
 import com.atguigu.beijingnewstest.domain.TabDetailPagerBean;
 import com.atguigu.beijingnewstest.utils.ConstantUtils;
+import com.atguigu.beijingnewstest.utils.DensityUtil;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.gson.Gson;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
+
+import java.util.List;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -39,6 +47,9 @@ public class TabDetailPager extends MenuDetailBasePager {
 
     private String url;
 
+    private List<TabDetailPagerBean.DataBean.TopnewsBean> topnews;
+    private int prePosition = 0;
+
     public TabDetailPager(Context context, NewsCenterBean.DataBean.ChildrenBean childrenBean) {
         super(context);
         this.childrenBean = childrenBean;
@@ -50,6 +61,32 @@ public class TabDetailPager extends MenuDetailBasePager {
         View view = View.inflate(context, R.layout.pager_tab_detail, null);
         ButterKnife.inject(this, view);
         //创建子类的视图
+
+        //监听页面的变化
+        viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                tvTitle.setText(topnews.get(position).getTitle());
+
+                //把之前设置为默认
+                llPointGroup.getChildAt(prePosition).setEnabled(false);
+                //当前的设置true
+                llPointGroup.getChildAt(position).setEnabled(true);
+
+                prePosition = position;
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
         return view;
     }
 
@@ -95,6 +132,67 @@ public class TabDetailPager extends MenuDetailBasePager {
         TabDetailPagerBean bean = new Gson().fromJson(response,TabDetailPagerBean.class);
 
         Log.e("TAG",""+bean.getData().getNews().get(0).getTitle());
+        topnews = bean.getData().getTopnews();
 
+        viewpager.setAdapter(new MyPagerAdapter());
+        //设置图片标题的文字
+        tvTitle.setText(topnews.get(prePosition).getTitle());
+
+        //添加图片viewPager的指示点
+        llPointGroup.removeAllViews();//把之前的先移除掉
+        for(int i = 0; i < topnews.size(); i++) {
+            ImageView point = new ImageView(context);
+            point.setBackgroundResource(R.drawable.point_selector);
+
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(DensityUtil.dip2px(context,8),DensityUtil.dip2px(context,8));
+            point.setLayoutParams(params);
+
+            if(i == 0) {
+                point.setEnabled(true);
+            }else{
+                point.setEnabled(false);
+                params.leftMargin = DensityUtil.dip2px(context,8);
+            }
+
+            //添加到线性布局中
+            llPointGroup.addView(point);
+        }
+
+    }
+
+    private class MyPagerAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return topnews.size();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            ImageView imageView = new ImageView(context);
+            imageView.setImageResource(R.drawable.pic_item_list_default);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            //设置网络图片
+
+            String imageUrl = ConstantUtils.BASE_URL + topnews.get(position).getTopimage();
+            Glide.with(context)
+                    .load(imageUrl)
+                    .placeholder(R.drawable.pic_item_list_default)
+                    .error(R.drawable.pic_item_list_default)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .into(imageView);
+            container.addView(imageView);
+
+            return imageView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
     }
 }
